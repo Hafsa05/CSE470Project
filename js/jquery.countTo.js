bbead2cfa1b6d93a20880c8461f3c08ddf4026d7
Hafsa05 <62322028+Hafsa05@users.noreply.github.com>
2022-04-19 21:01:30 +0600

    Update jquery.countTo.js

--- js/jquery.countTo.js
+++ js/jquery.countTo.js
@@ -3,7 +3,7 @@
         options = options || {};
 
         return $(this).each(function () {
-            // set options for current element
+            
             var settings = $.extend({}, $.fn.countTo.defaults, {
                 from: $(this).data('from'),
                 to: $(this).data('to'),
@@ -12,11 +12,9 @@
                 decimals: $(this).data('decimals')
             }, options);
 
-            // how many times to update the value, and how much to increment the value on each update
             var loops = Math.ceil(settings.speed / settings.refreshInterval),
                 increment = (settings.to - settings.from) / loops;
 
-            // references & variables that will change with each update
             var self = this,
                 $self = $(this),
                 loopCount = 0,
@@ -25,13 +23,10 @@
 
             $self.data('countTo', data);
 
-            // if an existing interval can be found, clear it first
             if (data.interval) {
                 clearInterval(data.interval);
             }
             data.interval = setInterval(updateTimer, settings.refreshInterval);
-
-            // initialize the element with the starting value
             render(value);
 
             function updateTimer() {
@@ -45,7 +40,7 @@
                 }
 
                 if (loopCount >= loops) {
-                    // remove the interval
+                    
                     $self.removeData('countTo');
                     clearInterval(data.interval);
                     value = settings.to;
@@ -64,14 +59,14 @@
     };
 
     $.fn.countTo.defaults = {
-        from: 0,               // the number the element should start at
-        to: 0,                 // the number the element should end at
-        speed: 1000,           // how long it should take to count between the target numbers
+        from: 0, // the number the element should start at
+        to: 0,   // the number the element should end at
+        speed: 1000,  // how long it should take to count between the target numbers
         refreshInterval: 100,  // how often the element should be updated
-        decimals: 0,           // the number of decimal places to show
-        formatter: formatter,  // handler for formatting the value before rendering
-        onUpdate: null,        // callback method for every time the element is updated
-        onComplete: null       // callback method for when the element finishes updating
+        decimals: 0, // the number of decimal places to show
+        formatter: formatter, // handler for formatting the value before rendering
+        onUpdate: null, // callback method for every time the element is updated
+        onComplete: null   // callback method for when the element finishes updating
     };
 
     function formatter(value, settings) {

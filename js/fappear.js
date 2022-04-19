5012b3f3f3dab1df26271acd4cecb6559186014e
Hafsa05 <62322028+Hafsa05@users.noreply.github.com>
2022-04-19 21:00:18 +0600

    Update fappear.js

--- js/fappear.js
+++ js/fappear.js
@@ -1,24 +1,11 @@
-/*
- * jQuery.fappear
- * https://github.com/bas2k/jquery.fappear/
- * http://code.google.com/p/jquery-fappear/
- *
- * Copyright (c) 2009 Michael Hixson
- * Copyright (c) 2012 Alexander Brovikov
- * Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
- */
 (function ($) {
     $.fn.fappear = function (fn, options) {
 
         var settings = $.extend({
 
-            //arbitrary data to pass to fn
             data: undefined,
-
-            //call fn only on the first fappear?
             one: true,
 
-            // X & Y accuracy
             accX: 0,
             accY: 0
 
@@ -27,31 +14,25 @@
         return this.each(function () {
 
             var t = $(this);
-
-            //whether the element is currently visible
             t.fappeared = false;
 
             if (!fn) {
-
-                //trigger the custom event
+                
                 t.trigger('fappear', settings.data);
                 return;
             }
 
             var w = $(window);
 
-            //fires the fappear event when appropriate
             var check = function () {
 
-                //is the element hidden?
                 if (!t.is(':visible')) {
 
-                    //it became hidden
                     t.fappeared = false;
                     return;
                 }
 
-                //is the element inside the visible window?
+                //the element inside the visible window
                 var a = w.scrollLeft();
                 var b = w.scrollTop();
                 var o = t.offset();
@@ -70,43 +51,31 @@
                     x + tw + ax >= a &&
                     x <= a + ww + ax) {
 
-                    //trigger the custom event
                     if (!t.fappeared) t.trigger('fappear', settings.data);
 
                 } else {
 
-                    //it scrolled out of view
                     t.fappeared = false;
                 }
             };
 
-            //create a modified fn with some additional logic
             var modifiedFn = function () {
 
-                //mark the element as visible
                 t.fappeared = true;
-
-                //is this supposed to happen only once?
                 if (settings.one) {
-
-                    //remove the check
                     w.unbind('scroll', check);
                     var i = $.inArray(check, $.fn.fappear.checks);
                     if (i >= 0) $.fn.fappear.checks.splice(i, 1);
                 }
 
-                //trigger the original fn
                 fn.apply(this, arguments);
             };
 
-            //bind the modified fn to the element
             if (settings.one) t.one('fappear', settings.data, modifiedFn);
             else t.bind('fappear', settings.data, modifiedFn);
 
-            //check whenever the window scrolls
             w.scroll(check);
-
-            //check whenever the dom changes
+            
             $.fn.fappear.checks.push(check);
 
             //check now
@@ -114,26 +83,25 @@
         });
     };
 
-    //keep a queue of fappearance checks
+    //queue check
     $.extend($.fn.fappear, {
 
         checks: [],
         timeout: null,
 
-        //process the queue
+        //process queue
         checkAll: function () {
             var length = $.fn.fappear.checks.length;
             if (length > 0) while (length--) ($.fn.fappear.checks[length])();
         },
 
-        //check the queue asynchronously
+        //check the queue
         run: function () {
             if ($.fn.fappear.timeout) clearTimeout($.fn.fappear.timeout);
             $.fn.fappear.timeout = setTimeout($.fn.fappear.checkAll, 20);
         }
     });
-
-    //run checks when these methods are called
+    
     $.each(['append', 'prepend', 'after', 'before', 'attr',
         'removeAttr', 'addClass', 'removeClass', 'toggleClass',
         'remove', 'css', 'show', 'hide'], function (i, n) {
@@ -152,10 +120,9 @@
 
 (function ($) {
     $.fn.countTo = function (options) {
-        // merge the default plugin settings with the custom options
+        
         options = $.extend({}, $.fn.countTo.defaults, options || {});
 
-        // how many times to update the value, and how much to increment the value on each update
         var loops = Math.ceil(options.speed / options.refreshInterval),
             increment = (options.to - options.from) / loops;
 

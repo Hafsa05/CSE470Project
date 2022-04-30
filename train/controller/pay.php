3fbeb81a0a80aa2082265a5792db94b04ebb0aba
Hafsa05 <62322028+Hafsa05@users.noreply.github.com>
2022-05-01 00:04:06 +0600

    Update pay.php

--- train/controller/pay.php
+++ train/controller/pay.php
@@ -1,5 +1,7 @@
 <?php
-  require_once 'session.php';
+namespace train\controller;
+
+require_once 'session.php';
   require_once '../constants.php';
   if (!isset($_SESSION['amount'], $_SESSION['email'])) {
     @session_destroy();
@@ -11,6 +13,41 @@
   $pay = curl_init();
   $email = $_SESSION['email'];
   $amount = $_SESSION['amount'] . "00";
+
+class pay{
+
+  public $number_of_ticket;
+  public $price_of_ticket;
+  public $payment_amount;
+    
+    public function setFirstClassPayment($n1, $n2) {
+     
+        $this->number_of_ticket = $n1;
+        $this->price_of_ticket = $n2;
+
+    }
+
+
+    public function getFirstClassPayment(){
+
+      $this->payment_amount = $this->number_of_ticket * $this->price_of_ticket;
+      return $this->payment_amount;
+    }
+
+    public function setSecondClassPayment($n1, $n2) {
+     
+      $this->number_of_ticket = $n1;
+      $this->price_of_ticket = $n2;
+
+  }
+
+
+  public function getFirstClassPayment(){
+
+    $this->payment_amount = $this->number_of_ticket * $this->price_of_ticket;
+    return $this->payment_amount;
+  }
+}
   
   curl_setopt_array($pay, array(
     CURLOPT_URL => "https://api.paystack.co/transaction/initialize",
